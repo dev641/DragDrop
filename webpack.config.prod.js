b1949f34859f973dcc35e08efbe46e67b4d20033
Devashish <dev641science@gmail.com>
2023-05-14 21:21:04 +0530

    DragDrop Upload

---
+++ webpack.config.prod.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const cleanPlugin = require("clean-webpack-plugin");
+module.exports = {
+  mode: "production",
+  entry: "./src/app.ts",
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
+  devtool: "hidden-source-map",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [new cleanPlugin.CleanWebpackPlugin()],
+};
